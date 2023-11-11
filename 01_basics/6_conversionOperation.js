42b7a84a5424925b82ad55ee66fe4fff38a09c65
Gaurav Halder <97378446+gaurav-halder@users.noreply.github.com>
2023-11-11 08:23:46 +0000

    Conversions

--- 01_basics/6_conversionOperation.js
+++ 01_basics/6_conversionOperation.js
@@ -1,7 +1,25 @@
-let score = "33"
+let score = "33abc"
 
 console.log(typeof score)
 console.log(typeof (score))
 
 let valueInNumber = Number(score)
 console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+//"33" => 33
+//"33abc" => NaN (Not a Number)
+// true => 1; false => 0;
+
+let isLoggedIn = 1
+let BooleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(BooleanIsLoggedIn)    //output - true
+
+//1 => true; 0 => false;
+//"" => false
+//"Gaurav" => true
+
+let someNumber = 33
+let Stringnumber = String(someNumber)
+console.log(Stringnumber)    //output 33
+console.log(typeof Stringnumber)    //output string
