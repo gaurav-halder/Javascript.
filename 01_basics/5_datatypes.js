56fe343c34cac6b9d5ebd79477e7fa6a931b825a
Gaurav Halder <97378446+gaurav-halder@users.noreply.github.com>
2023-11-11 05:33:43 +0000

    datatypes

---
+++ 01_basics/5_datatypes.js
@@ -0,0 +1,23 @@
+"use strict";  // treat all JS code as newar version of JS//
+
+// alert(3+3) //we are using nodejs not browser//
+
+console.log(3+3)
+console.log("Gaurav")
+
+let name ="Gaurav"
+let age ="21"
+let isLoggedIn = false
+
+// number =>2 to power 53
+// bigint 
+// string => ""
+//boolean => true/false
+//null => is a standalone value also a datatype - null is representation of empty value - empty
+//undefined => variable declare but value is not assigned.
+//symbol => used to define uniqueness
+//object
+
+console.log(typeof "Gaurav")
+console.log(typeof undefined)      //undefined - because its a type in javascript
+console.log(typeof null)    //object
--- 01_basics/test2.js
+++ 01_basics/test2.js
@@ -1,1 +1,12 @@
 console.log("Gaurav")
+const accountId =121231
+let accountemail = "aera@gmail.com"
+var accountcity ="kolkata"
+
+accountemail="heiaea.com"
+accountcity="jdafa"
+
+// perfer not to use var as it has a issue of block scope and fucntional scope//
+
+console.log(accountId);
+console.table([accountcity,accountemail]);
