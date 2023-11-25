5eb42b40c6bb3266fdd016c702c447fc1ca9c110
Gaurav Halder <97378446+gaurav-halder@users.noreply.github.com>
2023-11-25 07:27:24 +0000

    practise

---
+++ 01_basics/7_Operations.js
@@ -0,0 +1,22 @@
+let value = 3
+let negativeValue = -value
+console.log(negativeValue)
+
+console.log(2+2)
+console.log(2-3)
+console.log(2*3)
+console.log(2**3)
+console.log(2/3)
+console.log(2%3)
+
+let str1 ="Gaurav"
+let str2 =" Hello"
+let str3 = str1+str2
+console.log(str3)
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
+console.log( (2+3)+(2/3)+3)
+
---
+++ 01_basics/p_quest2.js
@@ -0,0 +1,8 @@
+const profile ={
+    username:"@gaurav",
+    isFollow: false,
+    followers: 569,
+    following: 4,
+};
+console.log(typeof profile);
+console.log(profile);
